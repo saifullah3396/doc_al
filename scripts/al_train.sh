c5c6d99e3ee91b1a4516ca1fd5f4b96e7d82d3da
saifullah3396 <saifullah3396@gmail.com>
2022-11-15 10:19:58 +0100

    update sh.

--- scripts/al_train.sh
+++ scripts/al_train.sh
@@ -3,6 +3,6 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 if [ -d "/netscratch/$USER_DIR/envs/xai_torch" ]; then
     source /netscratch/$USER_DIR/envs/xai_torch/bin/activate
 fi
-LOG_LEVEL=INFO python3 $SCRIPT_DIR/../src/al/trainers/train.py $@
+LOG_LEVEL=INFO python3 $SCRIPT_DIR/../src/al/trainers/al_train.py $@
 
 
